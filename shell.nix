4f2164c0e9b739661b937c07306eb1177621ae57
Millian Poquet <millian.poquet@inria.fr>
2018-05-01 21:15:22 +0200

    [misc] shell.nix

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{
+  pkgs ? import (
+    fetchTarball "https://github.com/NixOS/nixpkgs/archive/17.09.tar.gz") {},
+}:
+
+pkgs.stdenv.mkDerivation rec {
+  name = "netorcai-test-env";
+  env = pkgs.buildEnv { name = name; paths = buildInputs; };
+
+  buildInputs = [
+    #########
+    # Build #
+    #########
+    pkgs.go
+
+    ########
+    # Test #
+    ########
+    # Testsuite
+    pkgs.bats
+    # Client
+    pkgs.python36
+  ];
+}
