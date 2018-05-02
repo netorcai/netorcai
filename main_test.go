8ec91ebb574a01aea881b0a3b55a1ba2cc5cbd98
Millian Poquet <millian.poquet@inria.fr>
2018-05-02 17:05:32 +0200

    [coverage] main wrapper

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+// Greatly inspired from the following link.
+// https://www.cyphar.com/blog/post/20170412-golang-integration-coverage
+func TestRunMain(t *testing.T) {
+	var (
+		args []string
+	)
+
+	for _, arg := range os.Args {
+		switch {
+		case strings.HasPrefix(arg, "-test"):
+		case strings.HasPrefix(arg, "__bypass"):
+			args = append(args, strings.TrimPrefix(arg, "__bypass"))
+		default:
+			args = append(args, arg)
+		}
+	}
+	os.Args = args
+
+	// To retrieve coverage results, os.Exit must NOT be called
+	returnCode := mainReturnWithCode()
+	fmt.Println("Robin return code:", returnCode)
+}
