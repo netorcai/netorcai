4e1bffbe62622029ee8233266d4b9eae5c652143
Millian Poquet <millian.poquet@inria.fr>
2018-05-12 01:43:32 +0200

    [test] invalid DO_INIT_ACK messages

--- test/invalid_gl_test.go
+++ test/invalid_gl_test.go
@@ -60,3 +60,65 @@ func TestInvalidGlNoDoInitAck(t *testing.T) {
 		1000)
 	checkAllKicked(t, visuClients, regexp.MustCompile(`netorcai abort`), 1000)
 }
+
+func subtestBadDoInitAck(t *testing.T, doInitAckMessage string,
+	kickReasonMatcher *regexp.Regexp) {
+	proc, _, playerClients, visuClients, glClients := runNetorcaiAndAllClients(
+		t, 1000)
+	defer killallNetorcaiSIGKILL()
+
+	go func(glClient *Client) {
+		msg, err := waitReadMessage(glClient, 1000)
+		assert.NoError(t, err, "Could not read GLClient message (DO_INIT)")
+		checkDoInit(t, msg, 4, 100)
+
+		glClient.SendString(doInitAckMessage)
+
+		// Should be kicked
+		msg, err = waitReadMessage(glClient, 1000)
+		checkKick(t, msg, kickReasonMatcher)
+	}(glClients[0])
+
+	proc.inputControl <- `start`
+	_, err := waitOutputTimeout(regexp.MustCompile(`Game logic failed`),
+		proc.outputControl, 1000, false)
+	assert.NoError(t, err,
+		"Cannot read `Game logic failed` in netorcai output")
+
+	_, expRetCode := handleCoverage(t, 1)
+	retCode, err := waitCompletionTimeout(proc.completion, 1000)
+	assert.NoError(t, err, "netorcai did not complete")
+	assert.Equal(t, expRetCode, retCode, "Unexpected netorcai return code")
+
+	checkAllKicked(t, playerClients, regexp.MustCompile(`netorcai abort`),
+		1000)
+	checkAllKicked(t, visuClients, regexp.MustCompile(`netorcai abort`), 1000)
+}
+
+func TestInvalidGlBadDoInitAckNoMessageType(t *testing.T) {
+	subtestBadDoInitAck(t, `{}`,
+		regexp.MustCompile(`Field 'message_type' is missing`))
+}
+
+func TestInvalidGlBadDoInitAckBadMessageType(t *testing.T) {
+	subtestBadDoInitAck(t, `{"message_type":"LOGIN"}`,
+		regexp.MustCompile(`Received 'LOGIN' message type, `+
+			`while DO_INIT_ACK was expected`))
+}
+
+func TestInvalidGlBadDoInitAckNoGameState(t *testing.T) {
+	subtestBadDoInitAck(t, `{"message_type":"DO_INIT_ACK"}`,
+		regexp.MustCompile(`Field 'initial_game_state' is missing`))
+}
+
+func TestInvalidGlBadDoInitAckNonObjectGameState(t *testing.T) {
+	subtestBadDoInitAck(t, `{"message_type":"DO_INIT_ACK", `+
+		`"initial_game_state":0}`,
+		regexp.MustCompile(`Non-object value for field 'initial_game_state'`))
+}
+
+func TestInvalidGlBadDoInitAckNoAllClients(t *testing.T) {
+	subtestBadDoInitAck(t, `{"message_type":"DO_INIT_ACK", `+
+		`"initial_game_state":{}}`,
+		regexp.MustCompile(`Field 'all_clients' is missing`))
+}
