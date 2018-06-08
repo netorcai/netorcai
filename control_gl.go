d6d688a48f9f27568d1b0d4664808936960b2e5a
Millian Poquet <millian.poquet@inria.fr>
2018-06-08 02:51:23 +0200

    [code] split control.go into 3 files

---
+++ control_gl.go
@@ -0,0 +1,308 @@
+package netorcai
+
+import (
+	"encoding/json"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+type GameLogicClient struct {
+	client *Client
+	// Messages to aggregate from player clients
+	playerAction chan MessageDoTurnPlayerAction
+	// Control messages
+	start chan int
+}
+
+func handleGameLogic(glClient *GameLogicClient, globalState *GlobalState,
+	onexit chan int) {
+	// Wait for the game to start
+	select {
+	case <-glClient.start:
+		log.Info("Starting game")
+	case msg := <-glClient.client.incomingMessages:
+		globalState.Mutex.Lock()
+		if msg.err == nil {
+			Kick(glClient.client, "Received a game logic message but "+
+				"the game has not started")
+		} else {
+			Kick(glClient.client, fmt.Sprintf("Game logic error. %v",
+				msg.err.Error()))
+		}
+		globalState.GameLogic = globalState.GameLogic[:0]
+		globalState.Mutex.Unlock()
+		onexit <- 1
+		return
+	}
+
+	// Generate randomized player identifiers
+	globalState.Mutex.Lock()
+	initialNbPlayers := len(globalState.Players)
+	playerIDs := rand.Perm(len(globalState.Players))
+	for playerIndex, player := range globalState.Players {
+		player.playerID = playerIDs[playerIndex]
+	}
+
+	// Generate player information
+	playersInfo := []*PlayerInformation{}
+	for _, player := range globalState.Players {
+		info := &PlayerInformation{
+			PlayerID:      player.playerID,
+			Nickname:      player.client.nickname,
+			RemoteAddress: player.client.Conn.RemoteAddr().String(),
+			IsConnected:   true,
+		}
+		player.playerInfo = info
+		playersInfo = append(playersInfo, info)
+	}
+
+	// Sort player information by player_id
+	sort.Slice(playersInfo, func(i, j int) bool {
+		return playersInfo[i].PlayerID < playersInfo[j].PlayerID
+	})
+
+	// Send DO_INIT
+	err := sendDoInit(glClient, len(globalState.Players),
+		globalState.NbTurnsMax)
+	globalState.Mutex.Unlock()
+
+	if err != nil {
+		Kick(glClient.client, fmt.Sprintf("Cannot send DO_INIT. %v",
+			err.Error()))
+		onexit <- 1
+		return
+	}
+
+	// Wait for first turn (DO_INIT_ACK)
+	var msg ClientMessage
+	select {
+	case msg = <-glClient.client.incomingMessages:
+		if msg.err != nil {
+			Kick(glClient.client,
+				fmt.Sprintf("Cannot read DO_INIT_ACK. %v", msg.err.Error()))
+			onexit <- 1
+			return
+		}
+	case <-time.After(3 * time.Second):
+		Kick(glClient.client, "Did not receive DO_INIT_ACK after 3 seconds.")
+		onexit <- 1
+		return
+	}
+
+	doTurnAckMsg, err := readDoInitAckMessage(msg.content)
+	if err != nil {
+		Kick(glClient.client,
+			fmt.Sprintf("Invalid DO_INIT_ACK message. %v", err.Error()))
+		onexit <- 1
+		return
+	}
+
+	// Send GAME_STARTS to all clients
+	globalState.Mutex.Lock()
+	for _, player := range globalState.Players {
+		player.gameStarts <- MessageGameStarts{
+			MessageType:      "GAME_STARTS",
+			PlayerID:         player.playerID,
+			PlayersInfo:      []*PlayerInformation{},
+			NbPlayers:        initialNbPlayers,
+			NbTurnsMax:       globalState.NbTurnsMax,
+			DelayFirstTurn:   globalState.MillisecondsBeforeFirstTurn,
+			DelayTurns:       globalState.MillisecondsBetweenTurns,
+			InitialGameState: doTurnAckMsg.InitialGameState,
+		}
+	}
+
+	for _, visu := range globalState.Visus {
+		visu.gameStarts <- MessageGameStarts{
+			MessageType:      "GAME_STARTS",
+			PlayerID:         visu.playerID,
+			PlayersInfo:      playersInfo,
+			NbPlayers:        initialNbPlayers,
+			NbTurnsMax:       globalState.NbTurnsMax,
+			DelayFirstTurn:   globalState.MillisecondsBeforeFirstTurn,
+			DelayTurns:       globalState.MillisecondsBetweenTurns,
+			InitialGameState: doTurnAckMsg.InitialGameState,
+		}
+	}
+	globalState.Mutex.Unlock()
+
+	// Wait before really starting the game
+	log.WithFields(log.Fields{
+		"duration (ms)": globalState.MillisecondsBeforeFirstTurn,
+	}).Debug("Sleeping before first turn")
+	time.Sleep(time.Duration(globalState.MillisecondsBeforeFirstTurn) *
+		time.Millisecond)
+
+	// Order the game logic to compute a TURN (without any action)
+	turnNumber := 0
+	playerActions := make([]MessageDoTurnPlayerAction, 0)
+	sendDoTurn(glClient, playerActions)
+
+	for {
+		select {
+		case action := <-glClient.playerAction:
+			// A client sent its actions.
+			// Replace the current message from this player if it exists,
+			// and place it at the end of the array.
+			// This may happen if the client was late in a previous turn but
+			// catched up in current turn by sending two TURN_ACK.
+			actionFound := false
+			for actionIndex, act := range playerActions {
+				if act.PlayerID == action.PlayerID {
+					playerActions[len(playerActions)-1], playerActions[actionIndex] = playerActions[actionIndex], playerActions[len(playerActions)-1]
+					playerActions[len(playerActions)-1] = action
+					actionFound = true
+					break
+				}
+			}
+
+			if !actionFound {
+				// Append the action into the actions array
+				playerActions = append(playerActions, action)
+			}
+
+		case msg := <-glClient.client.incomingMessages:
+			// New message received from the game logic
+			if msg.err != nil {
+				Kick(glClient.client,
+					fmt.Sprintf("Cannot read DO_TURN_ACK. %v",
+						msg.err.Error()))
+				onexit <- 1
+				return
+			}
+
+			doTurnAckMsg, err := readDoTurnAckMessage(msg.content,
+				initialNbPlayers)
+			if err != nil {
+				Kick(glClient.client,
+					fmt.Sprintf("Invalid DO_TURN_ACK message. %v",
+						err.Error()))
+				onexit <- 1
+				return
+			}
+
+			turnNumber = turnNumber + 1
+			if turnNumber < globalState.NbTurnsMax {
+				// Forward the TURN to the clients
+				globalState.Mutex.Lock()
+				for _, player := range globalState.Players {
+					player.newTurn <- MessageTurn{
+						MessageType: "TURN",
+						TurnNumber:  turnNumber - 1,
+						GameState:   doTurnAckMsg.GameState,
+						PlayersInfo: []*PlayerInformation{},
+					}
+				}
+				for _, visu := range globalState.Visus {
+					visu.newTurn <- MessageTurn{
+						MessageType: "TURN",
+						TurnNumber:  turnNumber - 1,
+						GameState:   doTurnAckMsg.GameState,
+						PlayersInfo: playersInfo,
+					}
+				}
+				globalState.Mutex.Unlock()
+
+				// Trigger a new DO_TURN in some time
+				go func() {
+					log.WithFields(log.Fields{
+						"duration (ms)": globalState.MillisecondsBetweenTurns,
+					}).Debug("Sleeping before next turn")
+					time.Sleep(time.Duration(
+						globalState.MillisecondsBetweenTurns) *
+						time.Millisecond)
+
+					// Send current actions
+					sendDoTurn(glClient, playerActions)
+					// Clear actions array
+					playerActions = playerActions[:0]
+				}()
+			} else {
+				if doTurnAckMsg.WinnerPlayerID != -1 {
+					log.WithFields(log.Fields{
+						"winner player ID":      doTurnAckMsg.WinnerPlayerID,
+						"winner nickname":       playersInfo[doTurnAckMsg.WinnerPlayerID].Nickname,
+						"winner remote address": playersInfo[doTurnAckMsg.WinnerPlayerID].RemoteAddress,
+					}).Info("Game is finished")
+				} else {
+					log.Info("Game is finished (no winner!)")
+				}
+
+				// Send GAME_ENDS to all clients
+				globalState.Mutex.Lock()
+				for _, player := range globalState.Players {
+					player.gameEnds <- MessageGameEnds{
+						MessageType:    "GAME_ENDS",
+						WinnerPlayerID: doTurnAckMsg.WinnerPlayerID,
+						GameState:      doTurnAckMsg.GameState,
+					}
+				}
+				for _, visu := range globalState.Visus {
+					visu.gameEnds <- MessageGameEnds{
+						MessageType:    "GAME_ENDS",
+						WinnerPlayerID: doTurnAckMsg.WinnerPlayerID,
+						GameState:      doTurnAckMsg.GameState,
+					}
+				}
+
+				globalState.Mutex.Unlock()
+
+				// Leave the program
+				Kick(glClient.client, "Game is finished")
+				onexit <- 0
+				return
+			}
+		}
+	}
+}
+
+func sendDoInit(client *GameLogicClient, nbPlayers, nbTurnsMax int) error {
+	msg := MessageDoInit{
+		MessageType: "DO_INIT",
+		NbPlayers:   nbPlayers,
+		NbTurnsMax:  nbTurnsMax,
+	}
+
+	content, err := json.Marshal(msg)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Error("Cannot marshal JSON message")
+		return err
+	} else {
+		log.WithFields(log.Fields{
+			"nickname":       client.client.nickname,
+			"remote address": client.client.Conn.RemoteAddr(),
+			"content":        string(content),
+		}).Debug("Sending DO_INIT to game logic")
+		err = sendMessage(client.client, content)
+		return err
+	}
+}
+
+func sendDoTurn(client *GameLogicClient,
+	playerActions []MessageDoTurnPlayerAction) error {
+	msg := MessageDoTurn{
+		MessageType:   "DO_TURN",
+		PlayerActions: playerActions,
+	}
+
+	content, err := json.Marshal(msg)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Error("Cannot marshal JSON message")
+		return err
+	} else {
+		log.WithFields(log.Fields{
+			"nickname":       client.client.nickname,
+			"remote address": client.client.Conn.RemoteAddr(),
+			"content":        string(content),
+		}).Debug("Sending DO_TURN to game logic")
+		err = sendMessage(client.client, content)
+		return err
+	}
+}
---
+++ control_pv.go
@@ -0,0 +1,235 @@
+package netorcai
+
+import (
+	"encoding/json"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+)
+
+type PlayerOrVisuClient struct {
+	client     *Client
+	playerID   int
+	isPlayer   bool
+	gameStarts chan MessageGameStarts
+	newTurn    chan MessageTurn
+	gameEnds   chan MessageGameEnds
+	playerInfo *PlayerInformation
+}
+
+func handlePlayerOrVisu(pvClient *PlayerOrVisuClient,
+	globalState *GlobalState) {
+	turnBuffer := make([]MessageTurn, 0)
+	lastTurnNumberSent := -1
+
+	for {
+		select {
+		case gameStarts := <-pvClient.gameStarts:
+			// A game start has been received.
+			err := sendGameStarts(pvClient.client, gameStarts)
+			if err != nil {
+				KickLoggedPlayerOrVisu(pvClient, globalState,
+					fmt.Sprintf("Cannot send GAME_STARTS. %v", err.Error()))
+				return
+			}
+			pvClient.client.state = CLIENT_READY
+		case gameEnds := <-pvClient.gameEnds:
+			// A game end has been received.
+			err := sendGameEnds(pvClient.client, gameEnds)
+			if err != nil {
+				KickLoggedPlayerOrVisu(pvClient, globalState,
+					fmt.Sprintf("Cannot send GAME_ENDS. %v", err.Error()))
+				return
+			}
+
+			// Leave the client
+			Kick(pvClient.client, "Game is finished")
+			return
+		case turn := <-pvClient.newTurn:
+			// A new turn has been received.
+			if pvClient.client.state == CLIENT_READY {
+				// The client is ready, the message can be sent right now.
+				lastTurnNumberSent = turn.TurnNumber
+				err := sendTurn(pvClient.client, turn)
+				if err != nil {
+					KickLoggedPlayerOrVisu(pvClient, globalState,
+						fmt.Sprintf("Cannot send TURN. %v", err.Error()))
+					return
+				}
+				pvClient.client.state = CLIENT_THINKING
+			} else if pvClient.client.state == CLIENT_THINKING {
+				// The client is still computing something (its decisions for
+				// a player, or just updating its display for a visualization).
+				// The turn message is therefore buffered.
+				if len(turnBuffer) > 0 {
+					// Update the turn buffer with the new message.
+					turnBuffer[0] = turn
+				} else {
+					// Put the new message into the turn buffer.
+					turnBuffer = append(turnBuffer, turn)
+				}
+			}
+		case msg := <-pvClient.client.incomingMessages:
+			// A new message has been received from the player socket.
+			if msg.err != nil {
+				KickLoggedPlayerOrVisu(pvClient, globalState,
+					fmt.Sprintf("Cannot read TURN_ACK. %v", msg.err.Error()))
+				return
+			}
+			turnAckMsg, err := readTurnAckMessage(msg.content,
+				lastTurnNumberSent)
+			if err != nil {
+				KickLoggedPlayerOrVisu(pvClient, globalState,
+					fmt.Sprintf("Invalid TURN_ACK received. %v",
+						err.Error()))
+				return
+			}
+
+			// Check client state
+			if pvClient.client.state != CLIENT_THINKING {
+				KickLoggedPlayerOrVisu(pvClient, globalState,
+					"Received a TURN_ACK but the client state is not THINKING")
+				return
+			}
+
+			// Check turnNumber value
+			if turnAckMsg.turnNumber != lastTurnNumberSent {
+				KickLoggedPlayerOrVisu(pvClient, globalState,
+					fmt.Sprintf("Invalid TURN_ACK received: "+
+						"Expected turn_number=%v, got %v", lastTurnNumberSent,
+						turnAckMsg.turnNumber))
+			}
+
+			if pvClient.isPlayer {
+				// Forward the player actions to the game logic
+				globalState.Mutex.Lock()
+				if len(globalState.GameLogic) == 1 {
+					globalState.GameLogic[0].playerAction <- MessageDoTurnPlayerAction{
+						PlayerID:   pvClient.playerID,
+						TurnNumber: turnAckMsg.turnNumber,
+						Actions:    turnAckMsg.actions,
+					}
+				}
+				globalState.Mutex.Unlock()
+			}
+
+			// If a TURN is buffered, send it right now.
+			if len(turnBuffer) > 0 {
+				err := sendTurn(pvClient.client, turnBuffer[0])
+				if err != nil {
+					KickLoggedPlayerOrVisu(pvClient, globalState,
+						fmt.Sprintf("Cannot send TURN. %v", err.Error()))
+					return
+				}
+
+				// Empty turn buffer
+				turnBuffer = turnBuffer[0:]
+				pvClient.client.state = CLIENT_THINKING
+			} else {
+				pvClient.client.state = CLIENT_READY
+			}
+		}
+	}
+}
+
+func KickLoggedPlayerOrVisu(pvClient *PlayerOrVisuClient,
+	gs *GlobalState, reason string) {
+	// Remove the client from the global state
+	gs.Mutex.Lock()
+
+	if pvClient.isPlayer {
+		// Locate the player in the array
+		playerIndex := -1
+		for index, player := range gs.Players {
+			if player.client == pvClient.client {
+				playerIndex = index
+				break
+			}
+		}
+
+		if playerIndex == -1 {
+			log.Error("Could not remove player: Did not find it")
+		} else {
+			// Remove the player by placing it at the end of the slice,
+			// then reducing the slice length
+			gs.Players[len(gs.Players)-1], gs.Players[playerIndex] = gs.Players[playerIndex], gs.Players[len(gs.Players)-1]
+			gs.Players = gs.Players[:len(gs.Players)-1]
+		}
+	} else {
+		// Locate the visu in the array
+		visuIndex := -1
+		for index, visu := range gs.Visus {
+			if visu.client == pvClient.client {
+				visuIndex = index
+				break
+			}
+		}
+
+		if visuIndex == -1 {
+			log.Error("Could not remove visu: Did not find it")
+		} else {
+			// Remove the visu by placing it at the end of the slice,
+			// then reducing the slice length
+			gs.Visus[len(gs.Visus)-1], gs.Visus[visuIndex] = gs.Visus[visuIndex], gs.Visus[len(gs.Visus)-1]
+			gs.Visus = gs.Visus[:len(gs.Visus)-1]
+		}
+	}
+
+	gs.Mutex.Unlock()
+
+	// Kick the client
+	Kick(pvClient.client, reason)
+}
+
+func sendGameStarts(client *Client, msg MessageGameStarts) error {
+	content, err := json.Marshal(msg)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Error("Cannot marshal JSON message")
+		return err
+	} else {
+		log.WithFields(log.Fields{
+			"nickname":       client.nickname,
+			"remote address": client.Conn.RemoteAddr(),
+			"content":        string(content),
+		}).Debug("Sending GAME_STARTS to client")
+		err = sendMessage(client, content)
+		return err
+	}
+}
+
+func sendTurn(client *Client, msg MessageTurn) error {
+	content, err := json.Marshal(msg)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Error("Cannot marshal JSON message")
+		return err
+	} else {
+		log.WithFields(log.Fields{
+			"nickname":       client.nickname,
+			"remote address": client.Conn.RemoteAddr(),
+			"content":        string(content),
+		}).Debug("Sending TURN to client")
+		err = sendMessage(client, content)
+		return err
+	}
+}
+
+func sendGameEnds(client *Client, msg MessageGameEnds) error {
+	content, err := json.Marshal(msg)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Error("Cannot marshal JSON message")
+		return err
+	} else {
+		log.WithFields(log.Fields{
+			"nickname":       client.nickname,
+			"remote address": client.Conn.RemoteAddr(),
+			"content":        string(content),
+		}).Debug("Sending GAME_ENDS to client")
+		err = sendMessage(client, content)
+		return err
+	}
+}
