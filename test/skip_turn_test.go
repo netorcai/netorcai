c0c8bd7a75d0f5a7801d80f3f696b4c912ef15c7
Millian Poquet <millian.poquet@inria.fr>
2018-06-09 01:27:47 +0200

    [test] TestSkipFirstTurn

--- test/skip_turn_test.go
+++ test/skip_turn_test.go
@@ -56,3 +56,53 @@ func TestSkipOneTurnOverTwo(t *testing.T) {
 		regexp.MustCompile(`Game is finished`),
 		regexp.MustCompile(`Game is finished`))
 }
+
+// Skip first turn
+func turnAckSkipFirstTurn(turn, playerID int) string {
+	if turn == 0 {
+		time.Sleep(time.Duration(600) * time.Millisecond)
+	}
+	msg := fmt.Sprintf(`{"message_type": "TURN_ACK",
+        "turn_number": %v,
+        "actions": []}`, turn)
+	return msg
+}
+
+func checkTurnSkipFirstTurn(t *testing.T, msg map[string]interface{},
+	expectedNbPlayers, expectedTurnNumber int, isPlayer bool) int {
+
+	turn, err := netorcai.ReadInt(msg, "turn_number")
+	assert.NoError(t, err, "Cannot read 'turn_number'")
+
+	return turn
+}
+
+func checkDoTurnSkipFirstTurn(t *testing.T, msg map[string]interface{},
+	expectedNbPlayers, expectedTurnNumber int) []interface{} {
+
+	actions, err := netorcai.ReadArray(msg, "player_actions")
+	assert.NoError(t, err, "Cannot read 'player_actions'")
+
+	expectedPActionsLength := 1
+	if expectedTurnNumber < 1 {
+		expectedPActionsLength = 0
+	}
+
+	assert.Equal(t, expectedPActionsLength, len(actions),
+		"Unexpected array length for 'player_actions'. turn=%v",
+		expectedTurnNumber)
+	return actions
+}
+
+func TestSkipFirstTurn(t *testing.T) {
+	subtestHelloGlActiveClients(t, 1, 0,
+		4, 4, 4, 4,
+		0, 0,
+		DefaultHelloClientCheckGameStarts, checkTurnSkipFirstTurn,
+		DefaultHelloClientCheckGameEnds, checkDoTurnSkipFirstTurn,
+		DefaultHelloGLDoInitAck, DefaultHelloGlDoTurnAck,
+		turnAckSkipFirstTurn, DefaultHelloClientTurnAck,
+		regexp.MustCompile(`Game is finished`),
+		regexp.MustCompile(`Game is finished`),
+		regexp.MustCompile(`Game is finished`))
+}
